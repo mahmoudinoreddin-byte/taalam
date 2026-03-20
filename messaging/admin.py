da80dd41041205838bbd22af77f03b66b40f12bd
mahmoudinoreddin-byte <mahmoudinoreddin@gmail.com>
2026-03-20 14:16:17 +0100

    Delete messaging directory

--- messaging/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Message
-
-@admin.register(Message)
-class MessageAdmin(admin.ModelAdmin):
-    list_display = ['sender', 'recipient', 'subject', 'is_read', 'created_at']
-    list_filter = ['is_read']
