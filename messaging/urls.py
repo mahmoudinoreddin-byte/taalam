92c57b8fce99768e8eca3d8b49e294c5833af983
mahmoudinoreddin-byte <mahmoudinoreddin@gmail.com>
2026-03-21 16:45:38 +0100

    Delete messaging directory

--- messaging/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('inbox/', views.inbox, name='inbox'),
-    path('conversation/<int:conv_id>/', views.conversation, name='conversation'),
-    path('start/<str:username>/', views.start_conversation, name='start_conversation'),
-    path('delete/<int:conv_id>/', views.delete_conversation, name='delete_conversation'),
-]
