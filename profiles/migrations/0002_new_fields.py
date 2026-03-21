8558b5e806e04a4bff5a551d0729d2aea0117691
mahmoudinoreddin-byte <mahmoudinoreddin@gmail.com>
2026-03-21 14:10:47 +0100

    Update 0002_new_fields.py

--- profiles/migrations/0002_new_fields.py
+++ profiles/migrations/0002_new_fields.py
@@ -3,9 +3,9 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
     dependencies = [('profiles', '0001_initial')]
     operations = [
-        migrations.AddField(model_name='profile', name='phone', field=models.CharField(blank=True, max_length=30)),
-        migrations.AddField(model_name='profile', name='contact_email', field=models.EmailField(blank=True)),
-        migrations.AddField(model_name='sociallink', name='custom_label', field=models.CharField(blank=True, max_length=100)),
-        migrations.AddField(model_name='project', name='doc_url', field=models.URLField(blank=True)),
-        migrations.AddField(model_name='tool', name='doc_url', field=models.URLField(blank=True)),
+        migrations.AddField(model_name='profile', name='phone', field=models.CharField(blank=True, default='', max_length=30)),
+        migrations.AddField(model_name='profile', name='contact_email', field=models.EmailField(blank=True, default='')),
+        migrations.AddField(model_name='sociallink', name='custom_label', field=models.CharField(blank=True, default='', max_length=100)),
+        migrations.AddField(model_name='project', name='doc_url', field=models.URLField(blank=True, default='')),
+        migrations.AddField(model_name='tool', name='doc_url', field=models.URLField(blank=True, default='')),
     ]
